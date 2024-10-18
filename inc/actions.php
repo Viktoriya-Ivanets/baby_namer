eaec1da8f67dcb4debcfbc252a56c591b53a9f29
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 11:28:46 +0300

    actions.php - Create functions to render correct content dynamicly

---
+++ inc/actions.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Render index page
+ * @return void
+ */
+function index(): void
+{
+    render('index');
+}
+
+/**
+ * Render index page with validation errors
+ * @return void
+ */
+function form(): void
+{
+    render('index', ['errors' => getErrors()]);
+}
+
+/**
+ * Process form data
+ * @return void
+ */
+function proc(): void
+{
+    $name = htmlspecialchars(filter_input(INPUT_POST, 'name'));
+    $gender = htmlspecialchars(filter_input(INPUT_POST, 'gender'));
+
+    $errors = [];
+    if ($nameError = validateName($name)) {
+        $errors[] = $nameError;
+    }
+    if ($genderError = validateGender($gender, AVAILABLE_GENDERS)) {
+        $errors[] = $genderError;
+    }
+
+    if (count($errors) > 0) {
+        setErrors($errors);
+        redirect('form');
+    }
+
+    redirect('index');
+}
