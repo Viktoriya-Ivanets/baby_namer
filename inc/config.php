d5258d61ad56ba732a1b715a73351f352c62f52a
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 13:54:11 +0300

    config.php - Add error messages constant

--- inc/config.php
+++ inc/config.php
@@ -7,3 +7,9 @@ const DEFAULT_NAME_FILE = 'uploads' . DIRECTORY_SEPARATOR . 'names.json';
 const VIEWS_COMMON_DIR = 'views' . DIRECTORY_SEPARATOR . 'common' . DIRECTORY_SEPARATOR;
 const VIEWS_PAGES_DIR = 'views' . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR;
 const VIEWS_TEMPLATES_DIR = 'views' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
+const ERROR_MESSAGE = [
+    0 => 'Gender cannot be empty',
+    1 => 'Incorrect gender - must be male or female',
+    2 => 'The name cannot be empty',
+    3 => 'The name should be kept from 3 to 30 characters.'
+];
