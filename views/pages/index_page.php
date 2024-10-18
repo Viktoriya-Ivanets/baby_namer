956ad3c8a56d8eb1399f93622fa9527749a35d2d
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 13:46:16 +0300

    index_page.php - Refactore code due to formatted names array

--- views/pages/index_page.php
+++ views/pages/index_page.php
@@ -1,9 +1,3 @@
-<?php
-$names = readNamesFromFile();
-$maleNames = $names['male'] ?? [];
-$femaleNames = $names['female'] ?? [];
-?>
-
 <table class="table table-striped table-bordered mt-4 text-center">
     <caption class="text-center mb-2" style="caption-side: top;">Available names:</caption>
     <thead class="table-dark">
@@ -13,16 +7,11 @@ $femaleNames = $names['female'] ?? [];
         </tr>
     </thead>
     <tbody>
-        <?php
-        $maxRows = max(count($maleNames), count($femaleNames));
-        for ($i = 0; $i < $maxRows; $i++) :
-            $maleName = $maleNames[$i] ?? '';
-            $femaleName = $femaleNames[$i] ?? '';
-        ?>
+        <?php foreach ($namesForDisplay as $row) : ?>
             <tr>
-                <td><?= htmlspecialchars($maleName) ?></td>
-                <td><?= htmlspecialchars($femaleName) ?></td>
+                <td><?= htmlspecialchars($row['male']) ?></td>
+                <td><?= htmlspecialchars($row['female']) ?></td>
             </tr>
-        <?php endfor; ?>
+        <?php endforeach; ?>
     </tbody>
 </table>
