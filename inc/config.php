9a87bfb2f6899f4e3af45db4b73d5e56e8bfeb26
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 12:24:29 +0300

    config.php - Add constant for path to JSON file with names

--- inc/config.php
+++ inc/config.php
@@ -3,6 +3,7 @@
 const SITE_NAME = 'Baby Namer';
 const FOOTER = 'Created by - Viktoriia Danylenko';
 const AVAILABLE_GENDERS = ['male', 'female'];
+const DEFAULT_NAME_FILE = 'uploads' . DIRECTORY_SEPARATOR . 'names.json';
 const VIEWS_COMMON_DIR = 'views' . DIRECTORY_SEPARATOR . 'common' . DIRECTORY_SEPARATOR;
 const VIEWS_PAGES_DIR = 'views' . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR;
 const VIEWS_TEMPLATES_DIR = 'views' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
