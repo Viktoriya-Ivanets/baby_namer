f03415edb93e7746b2d8548ccceca3fa41f90c13
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 11:39:17 +0300

    index_page.php - Create main page component

---
+++ views/pages/index_page.php
@@ -0,0 +1,19 @@
+<table class="table table-striped table-bordered mt-4 text-center">
+    <caption class="text-center mb-2" style="caption-side: top;">Available names:</caption>
+    <thead class="table-dark">
+        <tr>
+            <th>Names for boys</th>
+            <th>Names for girls</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Alfred</td>
+            <td>Liza</td>
+        </tr>
+        <tr>
+            <td>Andre</td>
+            <td>Maria</td>
+        </tr>
+    </tbody>
+</table>
