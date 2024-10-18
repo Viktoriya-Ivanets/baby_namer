e5be1b18e5128061c5504c4a41d57a91e2514300
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 15:31:02 +0300

    form.php - Add data displaying due to invalid input

--- views/common/form.php
+++ views/common/form.php
@@ -11,15 +11,25 @@
 <form action="<?= getUrl('proc') ?>" method="post" class="w-100 mx-auto" novalidate>
     <div class="mb-3">
         <label for="name" class="form-label">Name:</label>
-        <input type="text" id="name" name="name" class="form-control" placeholder="Enter name" required>
+        <input
+            type="text"
+            id="name"
+            name="name"
+            class="form-control"
+            placeholder="Enter name"
+            required
+            value="<?= htmlspecialchars($oldInput['name'] ?? '') ?>">
     </div>
-
     <div class="mb-3">
         <label for="gender" class="form-label">Gender:</label>
         <select id="gender" name="gender" class="form-select" required>
-            <option value="" disabled selected>Choose gender</option>
+            <option value="" disabled <?= empty($oldInput['gender']) ? 'selected' : '' ?>>Choose gender</option>
             <?php foreach (AVAILABLE_GENDERS as $gender) : ?>
-                <option value="<?= $gender ?>"><?= ucfirst($gender) ?></option>
+                <option
+                    value="<?= $gender ?>"
+                    <?= isset($oldInput['gender']) && $oldInput['gender'] === $gender ? 'selected' : '' ?>>
+                    <?= ucfirst($gender) ?>
+                </option>
             <?php endforeach; ?>
         </select>
     </div>
