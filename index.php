b0bbb33539465e952cad1472d0d7e5177b0f581b
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 11:39:49 +0300

    index.php - Include needed files and init project

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Connecting the configuration file
+require_once 'inc/config.php';
+
+// Connecting files with functions
+require_once 'inc/actions.php';
+require_once 'inc/helpers.php';
+require_once 'inc/validators.php';
+
+init();
