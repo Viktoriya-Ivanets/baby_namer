a435df102c5946505fb01daf47f81dd9ab8dcfea
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 11:37:24 +0300

    form.php - Create form component

---
+++ views/common/form.php
@@ -0,0 +1,28 @@
+<?php if (!empty($errors)) : ?>
+    <div class="alert alert-danger">
+        <?php foreach ($errors as $error) : ?>
+            <p><?= $error ?></p>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
+
+<form action="<?= getUrl('proc') ?>" method="post" class="w-100 mx-auto" novalidate>
+    <div class="mb-3">
+        <label for="name" class="form-label">Name:</label>
+        <input type="text" id="name" name="name" class="form-control" placeholder="Enter name" required>
+    </div>
+
+    <div class="mb-3">
+        <label for="gender" class="form-label">Gender:</label>
+        <select id="gender" name="gender" class="form-select" required>
+            <option value="" disabled selected>Choose gender</option>
+            <?php foreach (AVAILABLE_GENDERS as $gender) : ?>
+                <option value="<?= $gender ?>"><?= ucfirst($gender) ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+
+    <div class="d-grid">
+        <input type="submit" value="Submit" class="btn btn-primary">
+    </div>
+</form>
