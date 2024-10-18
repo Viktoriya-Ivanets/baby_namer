107485cfcb7694e6aa432bbe5783c78a4f24934e
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 14:24:14 +0300

    validators.php - Refactore functions

--- inc/validators.php
+++ inc/validators.php
@@ -1,18 +1,30 @@
 <?php
 
 /**
+ * Check that field does not empty
+ * @param string $fieldValue
+ * @param string $errorMessage
+ * @return string|null
+ */
+function isNotEmpty(string $fieldValue, string $errorMessage): string | null
+{
+    if (empty($fieldValue)) {
+        return $errorMessage;
+    }
+    return null;
+}
+
+/**
  * Validate gender
  * @param string $gender
  * @param array $availableGenders ['male', 'female']
+ * @param string $errorMessage
  * @return string|null
  */
-function validateGender(string $gender, array $availableGenders): string | null
+function validateGender(string $gender, array $availableGenders, string $errorMessage): string | null
 {
-
-    if (empty($gender)) {
-        return ERROR_MESSAGE[0];
-    } else if (!in_array($gender, $availableGenders)) {
-        return ERROR_MESSAGE[1];
+    if (!in_array($gender, $availableGenders)) {
+        return $errorMessage;
     }
     return null;
 }
@@ -20,18 +32,43 @@ function validateGender(string $gender, array $availableGenders): string | null
 /**
  * Validate name by length
  * @param string $name
+ * @param string $errorMessage
  * @return string|null
  */
-function validateName(string $name): string | null
+function validateName(string $name, string $errorMessage): string | null
 {
-    if (empty($name)) {
-        return ERROR_MESSAGE[2];
-    }
-
     $length = strlen($name);
     if ($length < 3 || $length > 20) {
-        return ERROR_MESSAGE[3];
+        return $errorMessage;
     }
 
     return null;
+}
+
+/**
+ * Universal func for validating all field by defined validators
+ * @param array $fields - ['field_name' => 'value']
+ * @return array
+ */
+function validateFields(array $fields): array
+{
+    $errors = [];
+
+    foreach (VALIDATES_FOR_FIELD as $field => $validators) {
+        if (isset($fields[$field])) {
+            foreach ($validators as [$function, $params]) {
+                $params = array_map(function ($param) use ($fields, $field) {
+                    return $param === $field ? $fields[$field] : $param;
+                }, $params);
+
+                $error = call_user_func_array($function, $params);
+
+                if ($error) {
+                    $errors[$field][] = $error;
+                }
+            }
+        }
+    }
+
+    return $errors;
 }
