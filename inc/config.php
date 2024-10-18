6ee4391397e2caa5ba3e9418dee21377e8c97e36
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-18 11:23:49 +0300

    config.php - Add project constants

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+
+const SITE_NAME = 'Baby Namer';
+const FOOTER = 'Created by - Viktoriia Danylenko';
+const AVAILABLE_GENDERS = ['male', 'female'];
+const VIEWS_COMMON_DIR = 'views' . DIRECTORY_SEPARATOR . 'common' . DIRECTORY_SEPARATOR;
+const VIEWS_PAGES_DIR = 'views' . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR;
+const VIEWS_TEMPLATES_DIR = 'views' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
